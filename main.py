d67293cf74bdf05363283061744e26bf42940c9a
Zoe Qiwen <qiwenkolimntcode@gmail.com>
2026-03-23 12:23:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# def selamat():
+#     print("halo!")
+#     print("bagaimmana cuaca hari ini")
+# selamat()
+def selamat (nama):
+    print(f"halo {nama}")
+    print(f"kamu lagi ngapain {nama}?")
+selamat("qiwen")
+
+def hidup_dalam_mingggu(age):
+    total_minggu= 90 * 52
+    minggu_yang_sudah_dilalui = age * 52
+    sisa_minggu = total_minggu - minggu_yang_sudah_dilalui
+    print(f"you have {sisa_minggu} week left.")
+hidup_dalam_mingggu(8)
+def berbicara_debgan(nama, lokasi):
+    print(f"halo {nama}")
+    print(f"bagaimana keadaan di {lokasi}")
+berbicara_debgan("qiwen", "Tangerang")
